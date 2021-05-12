acc9d692628e40d1ce846a8c04353c016a97e915
Dennis <dennis.isvoranu@student.upt.ro>
2021-05-12 16:30:39 +0300

    REV-45
    continued working on score classes

--- src/main/java/Score/ScoreFile.java
+++ src/main/java/Score/ScoreFile.java
@@ -1,2 +1,31 @@
-package Score;public class ScoreFile {
+package Score;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ScoreFile {
+    public static int addScore(int x,String[] fileName)
+    {   try{
+        File restaurantPage= new File(fileName);
+        Scanner myRead=new Scanner(restaurantPage);
+        int current = 0;
+        int nrOfReviews = 0;
+        int[] read=new int[10];
+        while(myRead.hasNextLine()) {
+            read[current]=myRead.nextInt();
+            current++;
+        }
+        current=read[0];
+        nrOfReviews=read[1];
+        current = (current + x) / (nrOfReviews + 1);
+        return current;
+    } catch(FileNotFoundException exception){
+        System.out.println("An error has occured");
+        exception.printStackTrace();
+    }
+    }
+    public static int updateFile(int nr, int score) {
+
+
+    }
 }
