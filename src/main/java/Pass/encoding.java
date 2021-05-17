a3ceffb821dda71384776b80da388c6e5700cce5
Dennis <dennis.isvoranu@student.upt.ro>
2021-05-17 19:50:25 +0300

    REV-41
    Finished writing the code for encryption and saving

--- src/main/java/Pass/encoding.java
+++ src/main/java/Pass/encoding.java
@@ -1,9 +1,5 @@
 package Pass;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.security.spec.KeySpec;
-import java.util.Base64;
+
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
@@ -11,10 +7,14 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
+import java.util.Base64;
 import java.util.Scanner;
+
 public class encoding {
     private static final String S_key = "Random string";
     private static final String salt = "Another random string";
@@ -23,19 +23,13 @@ public class encoding {
         try {
             byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
             IvParameterSpec ivspec = new IvParameterSpec(iv);
-            SecretKeyFactory factory
-                    = SecretKeyFactory.getInstance(
-                    "PBKDF2WithHmacSHA256");
-            KeySpec spec = new PBEKeySpec(S_key.toCharArray(), salt.getBytes(),
-                    65536, 256);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(S_key.toCharArray(), salt.getBytes(), 65536, 256);
             SecretKey tmp = factory.generateSecret(spec);
             SecretKeySpec secretKey = new SecretKeySpec(
                     tmp.getEncoded(), "AES");
-
-            Cipher cipher = Cipher.getInstance(
-                    "AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey,
-                    ivspec);
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
             // Return encrypted string
             return Base64.getEncoder().encodeToString(
                     cipher.doFinal(strToEncrypt.getBytes(
@@ -49,31 +43,13 @@ public class encoding {
 
     public static String decrypt(String strToDecrypt) {
         try {
-
-            // Default byte array
-            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0};
-            // Create IvParameterSpec object and assign with
-            // constructor
-            IvParameterSpec ivspec
-                    = new IvParameterSpec(iv);
-
-            // Create SecretKeyFactory Object
-            SecretKeyFactory factory
-                    = SecretKeyFactory.getInstance(
-                    "PBKDF2WithHmacSHA256");
-
-            // Create KeySpec object and assign with
-            // constructor
-            KeySpec spec = new PBEKeySpec(
-                    S_key.toCharArray(), salt.getBytes(),
-                    65536, 256);
+            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(S_key.toCharArray(), salt.getBytes(), 65536, 256);
             SecretKey tmp = factory.generateSecret(spec);
-            SecretKeySpec secretKey = new SecretKeySpec(
-                    tmp.getEncoded(), "AES");
-
-            Cipher cipher = Cipher.getInstance(
-                    "AES/CBC/PKCS5PADDING");
+            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, secretKey,
                     ivspec);
             // Return decrypted string
@@ -88,20 +64,44 @@ public class encoding {
 
     public static void Save(String myfile, String password, String user) {
         try {
-            FileWriter myWriter = new FileWriter(myfile);
-            myWriter.write( user + "\n" + password);
+            FileWriter myWriter = new FileWriter(myfile, true);
+            myWriter.write(user + " " + password + "\n");
             myWriter.close();
+
         } catch (
                 IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
-    public static void runSave(String myfile, String password, String user)
-    {
 
+    public static void runSave(String myfile, String password, String user) {
+        String encoded = encrypt(password);
+        Save(myfile, encoded, user);
+    }
+
+    public static int checkDecrypt(String myfile, String user, String password) {
+        try {
+            File Obj = new File(myfile);
+            Scanner Reader = new Scanner(Obj);
+            String[] input = new String[2];
+                while (Reader.hasNextLine()){
+                    input = Reader.nextLine().split(" ");
+                if (input[0] == user) {
+                    if(password==decrypt(input[1]))
+                        return 1;
+                    else return 0;
+                }
+                }
+                Reader.close();
+            return 0;
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return -1;
+        }
     }
 }
-
 
 
